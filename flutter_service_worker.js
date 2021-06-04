8988353c807010b54cd740760657133c94b286f4
TonyTNguyen <42046528+TonyTNguyen@users.noreply.github.com>
2021-06-04 11:24:37 -0400

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,15 +5,16 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
-"assets/NOTICES": "876b77d1d5e548b20472beaaf4aabbee",
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"assets/NOTICES": "2376a26a983b24acd7f081e88232321a",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "e02991456344f9aaebed9ebc7f5d42cb",
-"/": "e02991456344f9aaebed9ebc7f5d42cb",
-"main.dart.js": "27fa630461ae02e558fbc18b3decc5a0",
+"index%20-%20Copy.html": "14f985086feac5ff74a3098b38bb8b51",
+"index.html": "74a984c6d911d5c9cb2a4b7dd7a86115",
+"/": "74a984c6d911d5c9cb2a4b7dd7a86115",
+"main.dart.js": "d44bca7f1624c72691c0598e0b38facb",
 "manifest.json": "416d3ae6cfaad2f75753a49b96d04ad2",
 "version.json": "b140f23d11d4996377e9615c0231d840"
 };
@@ -33,7 +34,7 @@ self.addEventListener("install", (event) => {
   return event.waitUntil(
     caches.open(TEMP).then((cache) => {
       return cache.addAll(
-        CORE.map((value) => new Request(value + '?revision=' + RESOURCES[value], {'cache': 'reload'})));
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
     })
   );
 });
